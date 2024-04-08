c00cbb3549e4d8da6216a0d0663be1e32ceba2a4
refokcer <refokcer@gmail.com>
2024-04-08 12:32:26 +0300

    Change map on real map and color

--- game_objects/impl/wall.py
+++ game_objects/impl/wall.py
@@ -2,5 +2,5 @@ from game_objects.game_object import GameObject
 
 
 class Wall(GameObject):
-    def __init__(self, in_surface, x, y, in_size: int, in_color=(0, 0, 255)):
+    def __init__(self, in_surface, x, y, in_size: int, in_color=(0, 138, 6)):
         super().__init__(in_surface, x * in_size, y * in_size, in_size, in_color)
